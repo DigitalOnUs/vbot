9b1ddb35de06a5f7253f882bc14e37f4aa2ec5ca
leofigy <angel.fig@yandex.com>
2020-01-06 20:47:08 -0600

    more skeletons ...

--- requests/requests.go
+++ requests/requests.go
@@ -2,13 +2,18 @@ package requests
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/sirupsen/logrus"
 	df "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+)
+
+// known intents
+const (
+	applications = "Applications"
+	secrets      = "Secrets" // Intent Creation/Update/Delete
 )
 
 // ErrorInvalidInput basic errors
@@ -22,5 +27,39 @@ func ConsumeIntents(c *gin.Context) {
 		c.Status(http.StatusBadRequest)
 		return
 	}
-	fmt.Println(wr.GetQueryResult().GetOutputContexts())
+
+	// What intent we are handling now is
+	// The actions are the context those must be added as part of the
+	// main activity
+	switch wr.QueryResult.Intent.GetDisplayName() {
+	case applications:
+		logrus.Infoln("Incomming application requests")
+		c.JSON(http.StatusOK, gin.H{})
+	case secrets:
+		logrus.Infoln("Accepted valued secrets")
+		c.JSON(http.StatusOK, gin.H{})
+	default:
+		logrus.Errorln("Unknown action")
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
+
+/*
+// just tell me what I have access for
+func listApplications(c *gin.Context, wr *df.WebhookRequest) {
+	// hard coded just for the testing
+	apps := []Application{Application{
+		"vault",
+	},
+		Application{
+			"consul",
+		},
+	}
+
+	// back format in the response
+	respose := &df.Fullfillment
+}
+
+// create the secret
+func createSecret(c *gin.Context, wr *df.WebhookRequest) {}
+*/
